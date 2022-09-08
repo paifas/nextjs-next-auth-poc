2abbe79e77bba7565d2d1688a862f81276a2a3d4
Bruno Silva <bruno.silva@diconium.com>
2022-09-08 17:28:17 +0100

    keep session when calling api from server (roles use case)

--- pages/api/roles.js
+++ pages/api/roles.js
@@ -1,15 +1,20 @@
 import { getSession } from 'next-auth/react';
 
 const handler = async (req, res) => {
-	const session = await getSession(req);
+	const session = await getSession({ req });
 	if (!session) {
 		return res.status(401).json({ error: 'Not authenticated' });
 	}
 
-	const { email } = req.query;
-
 	if (req.method === 'GET') {
-		return res.status(200).json({ data: 'hello ' + email });
+		const { email } = req.query;
+		if (email !== 'test@user.com') {
+			return res.status(403).json({ error: 'Not allowed' });
+		}
+
+		return res.status(200).json({
+			roles: ['admin', 'user', 'manager'],
+		});
 	}
 
 	return res.status(400).json({ error: 'Method not allowed' });
--- pages/profile.js
+++ pages/profile.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import { useSession } from 'next-auth/react';
+import { getSession, useSession } from 'next-auth/react';
 
-const Profile = () => {
+const Profile = ({ roles }) => {
 	const { data: session, status } = useSession();
-
 	if (status === 'authenticated') {
-		return <p>Welcome back {session.user.email} </p>;
+		return (
+			<div>
+				<p className="mb-4">
+					Welcome back <strong>{session.user.email}</strong> your roles are:
+				</p>
+				<div>
+					{roles.map((item, index) => (
+						<p key={index}>{item}</p>
+					))}
+				</div>
+			</div>
+		);
 	}
 	return <div className="text-4xl">Hello visitor!!!</div>;
+};
+
+export const getServerSideProps = async context => {
+	const session = await getSession({ req: context.req });
+
+	if (!session) {
+		return { props: {} };
+	}
+
+	const myHeaders = new Headers();
+	myHeaders.append('Cookie', context.req.headers.cookie);
+	const requestOptions = {
+		method: 'GET',
+		headers: myHeaders,
+	};
+
+	const endpoint =
+		process.env.API_URL + '/api/roles?email=' + session.user.email;
+	const response = await fetch(endpoint, requestOptions);
+
+	const data = await response.json();
+	return { props: { roles: data.roles } };
 };
 
 export default Profile;

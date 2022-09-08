a58450644eea5bdf5366ce6518c6fef1e7a08e6b
Bruno Silva <bruno.silva@diconium.com>
2022-09-08 13:30:09 +0100

    getSession use case, protect frontend routes

--- pages/auth.js
+++ pages/auth.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import AuthForm from '../components/auth-form';
+import { getSession } from 'next-auth/react';
 
 const Auth = () => {
 	return <AuthForm />;
+};
+
+export const getServerSideProps = async context => {
+	const session = await getSession({ req: context.req });
+
+	if (session) {
+		return {
+			redirect: {
+				destination: '/profile',
+				permanent: false,
+			},
+		};
+	}
+
+	return { props: { session } };
 };
 
 export default Auth;
